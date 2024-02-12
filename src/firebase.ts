bbb21f42bc6d39e6323d2f102129f696071cf7c2
Reza Zahedi <r.zahedi@gmail.com>
2024-02-12 14:31:59 -0800

    firebase init

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAh0b_V1qTmeQwKiHmcYkCVl6eEQMJVfdA",
+  authDomain: "highpaws-buddies.firebaseapp.com",
+  projectId: "highpaws-buddies",
+  storageBucket: "highpaws-buddies.appspot.com",
+  messagingSenderId: "723444273110",
+  appId: "1:723444273110:web:d72cdcb4012b242c201e77"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
