c8597f00e8ea1bc3cd04eabec46eec481ea24e9b
Reza Zahedi <r.zahedi@gmail.com>
2024-02-16 13:09:15 -0800

    feat: more user details in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
 
-  const { authUser, error, loading, logout } = useAuth()
+  const { authUser, profile, error, loading, logout } = useAuth()
 
   return (
     <div style={{border:'1px solid #666', padding:'20px', display:'flex', justifyContent:'space-between', alignItems:'center'}}>
@@ -12,7 +12,11 @@ export default function Header() {
       <div>
         {loading && <p>Loading...</p>}
         {error && <p>{error}</p>}
-        {authUser && <>{authUser.email} <button onClick={logout}>Logout</button></>}
+        {profile && <>
+          <Link to={`/${profile.id}`}>{profile.name}</Link><br />
+          {authUser?.email}<br />
+          <button onClick={logout}>Logout</button>
+        </>}
         {!authUser && <Link to="/login">Login</Link>}
       </div>
     </div>
