17a644f7f97aea2306f8c95c60080f290e3c4f62
Reza Zahedi <r.zahedi@gmail.com>
2024-02-18 23:36:52 -0800

    Using Firebase emulators in dev

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +19,8 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+if( import.meta.env.DEV ) {
+  connectFirestoreEmulator(db, "localhost", 8080)
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+}
