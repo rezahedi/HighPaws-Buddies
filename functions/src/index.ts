05a5055d09518c4f727cfd55e59a56eae03fa9ff
Reza Zahedi <r.zahedi@gmail.com>
2024-02-19 21:12:25 -0800

    posts cloud functions logic created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,13 +5,13 @@ import {
   // onDocumentUpdated,
   onDocumentDeleted,
   // Change,
-  // FirestoreEvent
+  // FirestoreEvent,
 } from "firebase-functions/v2/firestore";
-import * as admin from "firebase-admin";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore, FieldValue } from "firebase-admin/firestore";
 
-admin.initializeApp();
-
-const db = admin.firestore();
+initializeApp();
+const db = getFirestore();
 
 export const increaseFollowingStat =
   onDocumentCreated("profiles/{profileId}/following/{secondProfileId}", (event) => {
@@ -21,7 +21,7 @@ export const increaseFollowingStat =
     // Increase stats profiles/:profileId stats.following by 1
     const profileRef = db.doc(`profiles/${profileId}`);
     profileRef.update({
-      "stats.following": admin.firestore.FieldValue.increment(1),
+      "stats.following": FieldValue.increment(1),
     });
   });
 
@@ -33,7 +33,7 @@ export const increaseFollowerStat =
     // Increase stats profiles/:profileId stats.following by 1
     const profileRef = db.doc(`profiles/${profileId}`);
     profileRef.update({
-      "stats.followers": admin.firestore.FieldValue.increment(1),
+      "stats.followers": FieldValue.increment(1),
     });
   });
 
@@ -45,7 +45,7 @@ export const decreaseFollowingStat =
     // Decrease stats profiles/:profileId stats.following by 1
     const profileRef = db.doc(`profiles/${profileId}`);
     profileRef.update({
-      "stats.following": admin.firestore.FieldValue.increment(-1),
+      "stats.following": FieldValue.increment(-1),
     });
   });
 
@@ -57,6 +57,65 @@ export const decreaseFollowerStat =
     // Decrease stats profiles/:profileId stats.following by 1
     const profileRef = db.doc(`profiles/${profileId}`);
     profileRef.update({
-      "stats.followers": admin.firestore.FieldValue.increment(-1),
+      "stats.followers": FieldValue.increment(-1),
     });
   });
+
+
+// Post stats increase
+export const increasePostStat = onDocumentCreated("profiles/{profileId}/posts/{postId}", (event) => {
+  const profileId = event.params.profileId;
+  const profileRef = db.doc(`profiles/${profileId}`);
+  profileRef.update({
+    "stats.posts": FieldValue.increment(1),
+  });
+});
+
+// Post stats decrease
+export const decreasePostStat = onDocumentDeleted("profiles/{profileId}/posts/{postId}", (event) => {
+  const profileId = event.params.profileId;
+  const profileRef = db.doc(`profiles/${profileId}`);
+  profileRef.update({
+    "stats.posts": FieldValue.increment(-1),
+  });
+});
+
+// Fanout post to followers feed
+export const fanoutPost = onDocumentCreated(`profiles/{profileId}/posts/{postId}`, (event) => {
+  const postDoc = event.data;
+  if (!postDoc) return
+
+  const profileId = event.params.profileId;
+  const postId = event.params.postId;
+  const followersRef = db.collection(`profiles/${profileId}/followers`);
+  
+  // TODO: Add a limit in getting followers id, to make fanout in small batches like 500 followers at a time
+  // And keep track of the followers batches in a separate collection like profiles/:profileId/followersBatches
+
+  // Get list of followers id from profiles/:profileId/followers and fanout created post to their feed
+  followersRef.get().then((querySnapshot) => {
+    const bulkWriter = db.bulkWriter()
+    querySnapshot.forEach((doc) => {
+      const followerId = doc.id;
+      const followerFeedRef = db.doc(`profiles/${followerId}/feed/${postId}`);
+      bulkWriter.set(followerFeedRef, postDoc.data());
+    });
+    bulkWriter.close();
+  });
+})
+
+// Unfanout post from followers feed when post is deleted
+export const unfanoutPost = onDocumentDeleted(`profiles/{profileId}/posts/{postId}`, (event) => {
+  const profileId = event.params.profileId;
+  const postId = event.params.postId;
+  const followersRef = db.collection(`profiles/${profileId}/followers`);
+  followersRef.get().then((querySnapshot) => {
+    const bulkWriter = db.bulkWriter()
+    querySnapshot.forEach((doc) => {
+      const followerId = doc.id;
+      const followerFeedRef = db.doc(`profiles/${followerId}/feed/${postId}`);
+      bulkWriter.delete(followerFeedRef);
+    });
+    bulkWriter.close();
+  });
+})
