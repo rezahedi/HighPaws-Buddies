6e81f039a780506d8593ab8d64c7da0d8ea71433
Reza Zahedi <r.zahedi@gmail.com>
2024-02-27 23:32:44 -0800

    small style change

--- src/components/feed/NewPostBlock.tsx
+++ src/components/feed/NewPostBlock.tsx
@@ -24,7 +24,7 @@ export default function NewPostBlock( {profile}: {profile: profileProp} ) {
     <>
       <div className="flex gap-2 p-5 bg-white cursor-pointer" onClick={handleClick} onKeyDown={handleKeyDown} tabIndex={0} role="button">
         <img src={profile.avatars.buddy} alt="avatar" className="size-12 rounded-full" />
-        <div className="flex-grow flex items-center p-2 cursor-text text-gray-500 rounded-md border border-gray-300 bg-gray-100">What's happening?!</div>
+        <div className="flex-grow flex items-center p-2 cursor-text text-gray-500 rounded-md border border-gray-300 bg-gray-100 hover:border-gray-500">What's happening?!</div>
       </div>
       {showModal && <Modal onClose={()=>setShowModal(false)}><NewPost onCancel={()=>setShowModal(false)} /></Modal>}
     </>
