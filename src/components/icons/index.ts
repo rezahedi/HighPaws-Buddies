08385bd20250a7b3686dd1ee275edc3c9b117f3e
Reza Zahedi <r.zahedi@gmail.com>
2024-03-01 03:05:39 -0800

    Icons component created

---
+++ src/components/icons/index.ts
@@ -0,0 +1,35 @@
+import Home from '@/components/icons/Home'
+import Search from '@/components/icons/Search'
+import Notification from '@/components/icons/Notification'
+import Snooze from '@/components/icons/Snooze'
+import Message from '@/components/icons/Message'
+import Group from '@/components/icons/Group'
+import Profile from '@/components/icons/Profile'
+import Setting from '@/components/icons/Setting'
+import VerticalEllipsis from '@/components/icons/VerticalEllipsis'
+import Like from '@/components/icons/Like'
+import Comment from '@/components/icons/Comment'
+import Money from '@/components/icons/Money'
+import Close from '@/components/icons/Close'
+import Bin from '@/components/icons/Bin'
+import Pen from '@/components/icons/Pen'
+import PhotoPlus from '@/components/icons/PhotoPlus'
+
+export {
+  Home,
+  Search,
+  Notification,
+  Snooze,
+  Message,
+  Group,
+  Profile,
+  Setting,
+  VerticalEllipsis,
+  Like,
+  Comment,
+  Money,
+  Close,
+  Bin,
+  Pen,
+  PhotoPlus,
+}
