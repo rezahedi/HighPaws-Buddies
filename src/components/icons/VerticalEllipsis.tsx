c8bf56628ac860852128ee62e3682cdc383536e5
Reza Zahedi <r.zahedi@gmail.com>
2024-03-01 03:33:47 -0800

    SVG icons fill color

--- src/components/icons/VerticalEllipsis.tsx
+++ src/components/icons/VerticalEllipsis.tsx
@@ -1,7 +1,7 @@
 export default function VerticalEllipsis({className}: {className?: string}) {
   return (
     <div className={className}>
-      <svg width="24" height="24" viewBox="0 0 24 24">
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
         <circle cx="12" cy="12" r="1.5"></circle>
         <circle cx="12" cy="5" r="1.5"></circle>
         <circle cx="12" cy="19" r="1.5"></circle>
