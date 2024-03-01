fc653cd403d11839e0eb4f8b40bcfff63d5382f5
Reza Zahedi <r.zahedi@gmail.com>
2024-03-01 00:34:01 -0800

    fix: Switch between posts reloads the page

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -3,23 +3,25 @@ import { db } from '@/firebase';
 import { doc, onSnapshot } from 'firebase/firestore';
 import { useEffect, useState } from "react";
 import { postProp, returnPostProp } from "@/types/firestore";
-import { Header, Post } from "@/components";
+import { Header, Post, SidebarBanners, SidebarNav } from "@/components";
+import { PostSkeleton } from "@/components/skeletons";
 
 export default function PostPage() {
   const { userHandler, postId } = useParams()
   const [post, setPost] = useState<postProp | null>(null)
-  const [dataLoading, setDataLoading] = useState<boolean>(true)
+  const [dataLoading, setDataLoading] = useState<boolean>(false)
   const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
+    setPost(null)
     setDataLoading(true)
-    const docRef = doc(db, `profiles/${userHandler}/feed/${postId}`)
+    const docRef = doc(db, `profiles/${userHandler}/posts/${postId}`)
     const unsubscribe = onSnapshot(docRef, (doc) => {
       if ( !doc.exists() )
         return setError('Post not found!')
 
       const res: postProp | null = returnPostProp(doc)
-      setPost( res )
+      setPost(res)
     });
     setDataLoading(false)
 
@@ -29,9 +31,15 @@ export default function PostPage() {
   return (
     <>
       <Header />
-      {dataLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {post && <Post post={post} showComment />}
+      <div className='main'>
+        <SidebarNav />
+        <main className="wall">
+          {dataLoading && <PostSkeleton />}
+          {error && <p>{error}</p>}
+          {post && <Post post={post} showComment />}
+        </main>
+        <SidebarBanners />
+      </div>
     </>
   )
 }
