7822f896cdba03b6b005599267ed65af9f0ebd03
Reza Zahedi <r.zahedi@gmail.com>
2024-03-07 22:37:58 -0800

    remove unused code

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { Group, Home, Message, Money, Notification, Pen, Profile, Search } from '@/components/icons'
 import { useAuth } from '@/providers/auth'
 
