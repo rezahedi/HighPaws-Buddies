60c878977956c9e388ccf16a95c63986455346d3
Reza Zahedi <r.zahedi@gmail.com>
2024-03-07 23:06:45 -0800

    fix: remove unused code

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
 import Public from "@/pages/public"
 import Feed from "@/pages/feed"
 import Profile from "@/pages/profile"
-import New from "@/pages/new"
 import Notifications from "@/pages/notifications"
 import Discover from "@/pages/discover"
 
@@ -9,7 +8,6 @@ export {
   Public,
   Feed,
   Profile,
-  New,
   Notifications,
   Discover,
 }
