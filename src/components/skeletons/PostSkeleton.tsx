e4d5c5f95c090e5dd63a0cb43a5943cb67682caa
Reza Zahedi <r.zahedi@gmail.com>
2024-03-11 13:40:47 -0700

    chore: re-shape Post skeleton

--- src/components/skeletons/PostSkeleton.tsx
+++ src/components/skeletons/PostSkeleton.tsx
@@ -6,19 +6,25 @@ export default function PostSkeleton({count = 1}: {count?: number}) {
     <>
       {Array(count).fill(0).map((_, i) => (
         <div className='post' key={i}>
-          <header>
+          <header className='flex gap-5 items-center'>
             <Skeleton circle width={50} height={50} />
             <div>
-              <Skeleton width={350} />
-              <Skeleton width={300} />
+              <Skeleton width={100} />
+              <Skeleton width={150} />
             </div>
           </header>
           <div>
+            <Skeleton width={'80%'} />
+            <Skeleton width={'70%'} />
+          </div>
+          <div>
             <Skeleton height={300} />
           </div>
-          <footer>
+          <footer className='flex gap-2 items-center'>
             <Skeleton width={100} />
-            <Skeleton width={150} />
+            <div className='flex-grow'></div>
+            <Skeleton width={90} />
+            <Skeleton width={120} />
           </footer>
         </div>
       ))}
