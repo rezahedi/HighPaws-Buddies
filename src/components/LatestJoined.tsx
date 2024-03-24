984f82af10b2677f5f66d7a2b6ccb4f6c02057f3
Reza Zahedi <r.zahedi@gmail.com>
2024-03-24 13:20:06 -0700

    fix: fix key issue in LatestJoined

--- src/components/LatestJoined.tsx
+++ src/components/LatestJoined.tsx
@@ -1,21 +1,27 @@
 import { useEffect, useState } from 'react'
-import { profileProp, returnProfileProp } from '@/types/firestore'
-import { query, collection, limit, getDocs } from 'firebase/firestore'
+import { profileProp, returnProfileProp, } from '@/types/firestore'
+import { query, collection, where, limit, getDocs } from 'firebase/firestore'
 import { db } from '@/firebase'
 import Avatar from '@/components/post/Avatar'
 import { useAuth } from '@/providers/auth'
 
 export default function LatestJoined() {
   const { profile: authProfile } = useAuth()
-  const [docs, setDocs] = useState<(profileProp | null)[]>([])
+  const [docs, setDocs] = useState<(profileProp)[]>([])
   const docsLimit = 5
 
   useEffect(() => {
     (async () => {
-      const q = query(collection(db, `profiles`), limit(docsLimit))
+      // where doc id is not equal to authProfile id
+      const q = query(
+        collection(db, `profiles`), 
+        where('__name__', '!=', authProfile?.id),
+        limit(docsLimit)
+      )
       const querySnapshot = await getDocs(q)
       const docs: (profileProp | null)[] = querySnapshot.docs.map(doc => returnProfileProp(doc));
-      setDocs(docs)
+      const filteredDocs = docs.filter(doc => doc !== null) as profileProp[]
+      setDocs(filteredDocs)
     })()
   }, [])
 
@@ -24,20 +30,16 @@ export default function LatestJoined() {
     <div className='space-y-4 mt-4'>
       <h3 className='text-lg font-bold'>Latest joined users:</h3>
       <div className='flex gap-4 justify-center flex-wrap'>
-        {docs.map((doc, i) => (
-          <>
-          {doc && doc.id!==authProfile?.id && 
-            <Avatar key={i}
-              profileId={doc.id}
-              url={doc.avatars.buddy}
-              name={doc.name} withName
-              size='bg'
-              linked={true}
-              className='flex flex-col gap-1 items-center font-semibold'
-            />
-          }
-          </>
-        ))}
+        {docs.map((doc, i) => 
+          <Avatar key={i}
+            profileId={doc.id}
+            url={doc.avatars.buddy}
+            name={doc.name} withName
+            size='bg'
+            linked={true}
+            className='flex flex-col gap-1 items-center font-semibold'
+          />
+        )}
       </div>
     </div>
   )
