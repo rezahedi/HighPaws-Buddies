d2e2203c999c9c63b54440bc2be28e5c4055ca40
Reza Zahedi <r.zahedi@gmail.com>
2024-03-25 14:45:40 -0700

    fix Loading icon attr name issue

--- src/components/icons/Loading.tsx
+++ src/components/icons/Loading.tsx
@@ -2,8 +2,8 @@ export default function Loading({className}: {className?: string}) {
   return (
     <div className={className}>
       <svg viewBox="0 0 32 32" aria-hidden="true" fill="currentColor" className="animate-spin">
-        <circle cx="16" cy="16" r="14" fill="none" stroke="currentColor" stroke-width="4" strokeOpacity={.2}></circle>
-        <circle cx="16" cy="16" r="14" fill="none" stroke="currentColor" stroke-width="4" strokeDasharray={80} strokeDashoffset={60}></circle>
+        <circle cx="16" cy="16" r="14" fill="none" stroke="currentColor" strokeWidth="4" strokeOpacity={.2}></circle>
+        <circle cx="16" cy="16" r="14" fill="none" stroke="currentColor" strokeWidth="4" strokeDasharray={80} strokeDashoffset={60}></circle>
       </svg>
     </div>
   )
